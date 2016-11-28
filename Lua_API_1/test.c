b5a34905be16eac94d2682ff9869daea6bec7198
XiaoxiaoxiaoCoder <Wtu_Kevin@163.com>
2016-11-28 21:53:28 +0800

    Lua_API 第一篇

---
+++ Lua_API_1/test.c
@@ -0,0 +1,28 @@
+
+/*
+ * gcc test.c -llua
+ */
+
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    lua_State *L = lua_open();
+    printf("begininit : stack top = %d\n", lua_gettop(L));
+    
+    lua_pushnumber(L, 5);
+    lua_pushnumber(L, 4);
+    printf("pushargvs : stack top = %d\n", lua_gettop(L));
+    
+    lua_settop(L, 1);
+    printf("settopfun : stack top = %d\n", lua_gettop(L));
+    lua_settop(L, 0);
+    printf("settopfun : stack top = %d\n", lua_gettop(L));
+    
+    return 0;
+}
